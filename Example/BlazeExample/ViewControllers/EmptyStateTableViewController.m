cad06597bc7c3c537d2426fe798d0675d68fbcb4
Roy Derks <roy@synappz.nl>
2016-11-15 09:15:44 +0100

    Made BlazeTableViewController compatible with CustomView property within DZNDataSource.

--- Example/BlazeExample/ViewControllers/EmptyStateTableViewController.m
+++ Example/BlazeExample/ViewControllers/EmptyStateTableViewController.m
@@ -17,13 +17,24 @@
 -(void)viewDidLoad
 {
     [super viewDidLoad];
-    
     //Empty state
     self.emptyImage = [UIImage imageNamed:@"Blaze_Logo"];
     self.emptyBackgroundColor = [UIColor whiteColor];
     self.emptyVerticalOffset = -100.0f;
     self.emptyTitle = @"This is an example text for an empty state, you can provide colors, atributed text, image, background color, etc.\nCredits to DZNEmptyDataSet!";
     self.emptyTitleAttributes = @{NSFontAttributeName:[UIFont systemFontOfSize:18.0f weight:UIFontWeightLight], NSForegroundColorAttributeName:[UIColor darkGrayColor]};
+    self.emptyBackgroundColor = [UIColor redColor];
+    
+    //Empty state custom view
+    /*
+    UIView *view = [UIView new];
+    self.emptyVerticalOffset = -400.0f;
+    UILabel *lbl = [[UILabel alloc] initWithFrame:CGRectMake(50, 200, 250, 50)];
+    lbl.text = @"Custom label!";
+    lbl.backgroundColor = [UIColor yellowColor];
+    [view addSubview:lbl];
+    self.emptyCustomView = view;
+     */
 }
 
 @end
